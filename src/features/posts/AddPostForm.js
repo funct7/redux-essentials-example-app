d2805ad6c31a95fcd43ab85a26af61e9662fd00d
Joshua Park <wmpark@knowre.com>
2020-08-04 16:02:10 +0900

    Add: PostAuthor component

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { postAdded } from './postsSlice';
 
@@ -7,18 +7,27 @@ export const AddPostForm = () => {
 	// ??: How do components know about the state? Where is the canonical document on what states are available?
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
+	const [userId, setUserId] = useState('');
 
 	const dispatch = useDispatch();
+	const users = useSelector(state => state.users);
 
 	const onTitleChanged = e => setTitle(e.target.value);
 	const onContentChanged = e => setContent(e.target.value);
+	const onAuthorChanged = e => setUserId(e.target.value)
 
 	const onSavePostClicked = () => {
 		if (!title || !content) return;
-		dispatch(postAdded(title, content));
+		dispatch(postAdded(title, content, userId));
 		setTitle('');
 		setContent('');
 	};
+
+	const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+	const userOptions = users.map(user => (
+		<option key={user.id} value={user.id}>{user.name}</option>
+	));
 
 	return (
 		<section>
@@ -26,10 +35,15 @@ export const AddPostForm = () => {
 			<form>
 				<label htmlFor="postTitle">Post Title:</label>
 				<input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
+				<label htmlFor="postAuthor">Author:</label>
+				<select id="postAuthor" value="userId" onChange={onAuthorChanged}>
+					<option value=""></option>
+					{userOptions}
+				</select>
 				<label htmlFor="postContent">Content:</label>
 				<textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
-				<button type="button" onClick={onSavePostClicked}>Save Post</button>
+				<button type="button" onClick={onSavePostClicked} disabled={!canSave}>Save Post</button>
 			</form>
-		</section>
+		</section >
 	);
 };
